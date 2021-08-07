1bbe1bb30f917e0d68f2fb49ee73df13fe8c877d
仁志 <legend1world@gmail.com>
2021-08-07 21:04:52 +0900

    up

--- search/data.js
+++ search/data.js
@@ -14,16 +14,28 @@ var data = [
     body: "Posts"
   },
   {
+    url: "https://dpnote.netlify.app/tags/",
+    title: "Tags",
+    date: "2021-08-05T00:00:00Z",
+    body: "Tags"
+  },
+  {
+    url: "https://dpnote.netlify.app/tags/%E5%8D%97%E6%B5%B7%E3%83%88%E3%83%A9%E3%83%95%E5%9C%B0%E9%9C%87%E5%AF%BE%E7%AD%96/",
+    title: "南海トラフ地震対策",
+    date: "2021-08-05T00:00:00Z",
+    body: "南海トラフ地震対策"
+  },
+  {
     url: "https://dpnote.netlify.app/post/destructionrate/",
     title: "被害率曲線をご存じですか？",
     date: "2021-08-05T00:00:00Z",
     body: "被害率曲線をご存じですか？ 今日は、被害率曲線のお話です。 南海トラフ地震対策を考える② 「被害率曲線をご存知ですか？」 耐震化は、まずは1981年がポイントです。 内閣府の資料の木造建物の被害率曲線をみると、1981年以後と以前で震度6以上から全壊率に \u0026quot;開き\u0026quot; が出ています。 家の築年数を調べてみましょう。また、自治体に耐震診断を依頼できます。 pic.twitter.com/CnU0SDLYwi \u0026mdash; ひとし 防災エンジニア (@hitoshi2sl) July 15, 2021"
   },
   {
-    url: "https://dpnote.netlify.app/tags/",
-    title: "Tags",
-    date: "2021-08-04T00:00:00Z",
-    body: "Tags"
+    url: "https://dpnote.netlify.app/tags/%E9%A6%96%E9%83%BD%E7%9B%B4%E4%B8%8B%E5%9E%8B%E5%9C%B0%E9%9C%87%E5%AF%BE%E7%AD%96/",
+    title: "首都直下型地震対策",
+    date: "2021-08-05T00:00:00Z",
+    body: "首都直下型地震対策"
   },
   {
     url: "https://dpnote.netlify.app/post/hazardmap/",
@@ -38,10 +50,10 @@ var data = [
     body: "東京都災害時給水ステーションをマップ化してみた。 東京都のオープンデータを用いて、災害時給水ステーションをマップ化してみました。 首都直下型地震対策を考える⑦ 「東京都災害時給水ステーションをマップ化」 人が生きていく上で何よりも「水」が大切‼️ 東京都のオープンデータを利用。地域に偏りが無いことが分かります。日本全国のオープンデータ化を求めます！ 👇マップURLはこちらhttps://t.co/QTo91hZfJ6 \u0026mdash; ひとし 防災エンジニア (@hitoshi2sl) July 25, 2021"
   },
   {
-    url: "https://dpnote.netlify.app/tags/%E9%A6%96%E9%83%BD%E7%9B%B4%E4%B8%8B%E5%9E%8B%E5%9C%B0%E9%9C%87%E5%AF%BE%E7%AD%96/",
-    title: "首都直下型地震対策",
+    url: "https://dpnote.netlify.app/tags/%E7%81%BD%E5%AE%B3%E3%83%87%E3%83%BC%E3%82%BF%E5%8F%AF%E8%A6%96%E5%8C%96/",
+    title: "災害データ可視化",
     date: "2021-08-04T00:00:00Z",
-    body: "首都直下型地震対策"
+    body: "災害データ可視化"
   },
   {
     url: "https://dpnote.netlify.app/post/tokyodaytime/",
@@ -66,12 +78,6 @@ var data = [
     title: "東京都火災発生件数を可視化しました",
     date: "2021-08-01T00:00:00Z",
     body: "東京都火災発生件数を可視化しました 東京都のオープンデータを利用して、都内の火災発生件数を可視化しました。 令和2年火災年報によると全国で発生した火災のうち 出火原因1位:たばこ、2位:たき火 とのことです。 知識の整理 １．避難時はブレーカーを落とす　→　通電火災防止 ２．家の周りに燃える物を置かない　→　放火防止 ３．火災報知器の取り付け・点検 東京都火災件数を可視化 令和2年火災年報によると全国で発生した火災のうち 出火原因1位:たばこ、2位:たき火とのこと 知識の整理 ❶避難時はブレーカーを落とす(通電火災防止) ❷家の周りに燃える物を置かない(放火防止) ❸火災報知器の取り付け・点検 少しずつ、地図の可視化にも慣れてきた！ pic.twitter.com/hz6k877rru \u0026mdash; ひとし 防災エンジニア (@hitoshi2sl) June 27, 2021"
-  },
-  {
-    url: "https://dpnote.netlify.app/tags/%E7%81%BD%E5%AE%B3%E3%83%87%E3%83%BC%E3%82%BF%E5%8F%AF%E8%A6%96%E5%8C%96/",
-    title: "災害データ可視化",
-    date: "2021-08-01T00:00:00Z",
-    body: "災害データ可視化"
   },
   {
     url: "https://dpnote.netlify.app/search/data.js",
