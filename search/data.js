22b38e3dbf53aa5a69e87659202ac74a0a69530d
仁志 <legend1world@gmail.com>
2021-08-07 14:00:20 +0900

    up

--- search/data.js
+++ search/data.js
@@ -4,32 +4,32 @@ var data = [
   {
     url: "https://dpnote.netlify.app/",
     title: "DP Note",
-    date: "2021-07-17T00:00:00Z",
+    date: "2021-08-07T00:00:00Z",
     body: "DP Note"
   },
   {
     url: "https://dpnote.netlify.app/post/",
     title: "Posts",
-    date: "2021-07-17T00:00:00Z",
+    date: "2021-08-07T00:00:00Z",
     body: "Posts"
   },
   {
     url: "https://dpnote.netlify.app/tags/",
     title: "Tags",
-    date: "2021-07-17T00:00:00Z",
+    date: "2021-08-07T00:00:00Z",
     body: "Tags"
   },
   {
-    url: "https://dpnote.netlify.app/tags/%E9%A6%96%E9%83%BD%E7%9B%B4%E4%B8%8B%E5%9E%8B%E5%9C%B0%E9%9C%87/",
-    title: "首都直下型地震",
-    date: "2021-07-17T00:00:00Z",
-    body: "首都直下型地震"
+    url: "https://dpnote.netlify.app/post/tokyofire/",
+    title: "東京都火災発生件数を可視化しました",
+    date: "2021-08-07T00:00:00Z",
+    body: "東京都火災発生件数を可視化しました 東京都のオープンデータを利用して、都内の火災発生件数を可視化しました。 令和2年火災年報によると全国で発生した火災のうち 出火原因1位:たばこ、2位:たき火 とのことです。 知識の整理 ➊ 避難時はブレーカーを落とす　→　通電火災防止 ❷ 家の周りに燃える物を置かない　→　放火防止 ❸ 火災報知器の取り付け・点検"
   },
   {
-    url: "https://dpnote.netlify.app/post/1/",
-    title: "首都直下型地震対策を考える１",
-    date: "2021-07-17T00:00:00Z",
-    body: "首都直下型地震対策を考える１ 東京都の昼間人口 Exel VBAで最終行、最終列を取得する方法です。 範囲指定で1行目～セルの値のある最終行まで指定したい場合などに使用します。 僕もよく忘れるので、記事にしました。 コードは以下のとおりです。"
+    url: "https://dpnote.netlify.app/tags/%E7%81%BD%E5%AE%B3%E3%83%87%E3%83%BC%E3%82%BF%E5%8F%AF%E8%A6%96%E5%8C%96/",
+    title: "災害データ可視化",
+    date: "2021-08-07T00:00:00Z",
+    body: "災害データ可視化"
   },
   {
     url: "https://dpnote.netlify.app/tags/%E5%8D%97%E6%B5%B7%E3%83%88%E3%83%A9%E3%83%95%E5%9C%B0%E9%9C%87/",
@@ -54,12 +54,6 @@ var data = [
     title: "首都直下型地震対策を考える１",
     date: "2021-07-08T00:00:00Z",
     body: "首都直下型地震対策を考える１ Exel VBAで最終行、最終列を取得する方法です。 範囲指定で1行目～セルの値のある最終行まで指定したい場合などに使用します。 僕もよく忘れるので、記事にしました。 コードは以下のとおりです。"
-  },
-  {
-    url: "https://dpnote.netlify.app/tags/%E7%81%BD%E5%AE%B3%E3%83%87%E3%83%BC%E3%82%BF%E5%8F%AF%E8%A6%96%E5%8C%96/",
-    title: "災害データ可視化",
-    date: "2021-07-07T00:00:00Z",
-    body: "災害データ可視化"
   },
   {
     url: "https://dpnote.netlify.app/post/3/",
